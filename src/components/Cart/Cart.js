00b3b687944575ab245608c2183ec46504ba6504
prasanjitsarker3 <sarkerprasanjit379@gmail.com>
2021-08-01 16:34:09 +0600

    Code Push

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
 
 const Cart = (props) => {
     const cart = props.cart;
@@ -7,7 +7,7 @@ const Cart = (props) => {
     let total =0;
     for (let i = 0; i< cart.length; i++) {
         const product = cart[i];
-        total = total + product.price;
+        total = total + product.price * product.quantity;
         
     }
     let shipping = 0;
@@ -37,9 +37,9 @@ const Cart = (props) => {
               <p>Tax + Vat: {tax}</p>
              <p>Total Price:{grandTotal}</p>
              <br />
-             <Link to="/reviews">
-                 <button className="main-button">Review Oder</button>
-             </Link>
+             {
+                 props.children
+             }
         </div>
     );
 };
--- src/components/ReviewItem/ReviewItem.js
+++ src/components/ReviewItem/ReviewItem.js
@@ -2,19 +2,22 @@ import React from 'react';
 
 const ReviewItem = (props) => {
    
-    const {name,quantity} = props.product;
+    const {name,quantity,key,price} = props.product;
     const reviewItemStyle = {
         borderBottom: '1px solid lightgray',
         marginBottom: '5px',
         paddingBottom: '5px',
-        marginLeft: '200px'
+        marginLeft: '200px' 
     }
     return (
         <div style={reviewItemStyle} className="review-item">
             <h4 className="product-name">{name}</h4>
             <p>Quantity: {quantity}</p>
+            <p><small>Price:$ {price}</small></p>
             <br />
-            <button className="main-button"> Remove</button>
+            <button className="main-button"
+            onClick={() =>props.removeProduct(key)}
+            > Remove</button>
         </div>
     );
 };
